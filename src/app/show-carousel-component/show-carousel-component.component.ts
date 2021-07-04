378acfb65cfc214e4824d2b25698237e5c026890
rajthilakam <rajthilakam@gmail.com>
2021-07-03 22:47:19 -0700

    Changed show

--- src/app/show-carousel-component/show-carousel-component.component.ts
+++ src/app/show-carousel-component/show-carousel-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ishowdata } from '../ishowdata';
+import { ShowdataService } from '../showdata.service';
 import { CarouselService } from '../carousel.service';
 import { Icarousel } from '../icarousel';
 
@@ -9,12 +11,30 @@ import { Icarousel } from '../icarousel';
 })
 export class ShowCarouselComponentComponent implements OnInit {
   current: Icarousel[] = [];
+  showdata:Ishowdata[] = []
+  action:Ishowdata[] = []
+  adventure:Ishowdata[] = []
+  thriller:Ishowdata[] = []
+  comedy:Ishowdata[] = []
+  sci:Ishowdata[] = []
+  romance:Ishowdata[] = []
+  drama:Ishowdata[] = []
 
-  constructor(private carouselService: CarouselService) { }
+
+  constructor(private showdataservice:ShowdataService) { }
 
   ngOnInit(): void {
-    this.carouselService.getCarouselServiceData().subscribe(data => {
-      this.current = data
+
+    this.showdataservice.getshowdata().subscribe(data => {
+     this.showdata = data
+     this.action = this.showdata.filter(data => data.genres.includes('Action'))
+     this.adventure = this.showdata.filter(data => data.genres.includes('Adventure'))
+     this.thriller = this.showdata.filter(data => data.genres.includes('Thriller'))
+     this.comedy = this.showdata.filter(data => data.genres.includes('Comedy'))
+     this.sci = this.showdata.filter(data => data.genres.includes('Science-Fiction'))
+     this.romance = this.showdata.filter(data => data.genres.includes('Romance'))
+     this.drama = this.showdata.filter(data => data.genres.includes('Drama'))
+
     })
   }
 
